40624d7fec2835f1855ef8602cac452109d4d77b
Per Rutquist <per.rutquist@gmail.com>
2019-12-03 08:38:22 +0100

    day 3 solution

--- day3.jl
+++ day3.jl
@@ -17,8 +17,7 @@ function wirepath(ps)
     x = 0
     y = 0
     q = 0
-    for p in ps
-        (d, l) = p
+    for (d, l) in ps
         (xn, yn) = if d == 'L'
             (x - l, y)
         elseif d == 'U'
@@ -41,14 +40,18 @@ manhattan(v) = sum(abs.(v))
 function solve(data)
     q1 = wirepath(data[1])
     q2 = wirepath(data[2])
-    i = findall((q1 .> 0) .& (q2 .> 0))
+
+    # i = findall((q1 .> 0) .& (q2 .> 0)) # TODO: Make a PR to OffsetArrays.jl to make this as fast as the below code.
+
+    offset(ix) = CartesianIndex(Tuple(ix) .+ first.(axes(q1)) .- 1)
+    i = offset.(findall((parent(q1) .> 0) .& (parent(q2) .> 0)))
 
     (minimum(manhattan.(Tuple.(i))), minimum(q1[i] .+ q2[i]))
 end
 
-# data1 = scan.(["R8,U5,L5,D3", "U7,R6,D4,L4"])
-# @test solve(data1) == (6, 30)
-# data2 = scan.(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"])
-# @test solve(data2) == (135, 410)
- 
+data1 = scan.(["R8,U5,L5,D3", "U7,R6,D4,L4"])
+@test solve(data1) == (6, 30)
+data2 = scan.(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"])
+@test solve(data2) == (135, 410)
+
 @show solve(data)
