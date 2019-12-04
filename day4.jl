020ab75d3a6a8af96c87f03414374f068f53114e
Per Rutquist <per.rutquist@gmail.com>
2019-12-04 07:46:01 +0100

    day 4 solution

---
+++ day4.jl
@@ -0,0 +1,21 @@
+function countpasswords(range)
+    s1 = 0
+    s2 = 0
+    for i in range
+        digit(k) = i ÷ 10^k % 10
+        d = digit.((5,4,3,2,1,0))
+        if all(d[2:6] .>= d[1:5])
+            if any(d[1:5] .== d[2:6])
+                s1 += 1
+                v = (-1, d..., -1)
+                p(k) = v[k] != v[k+1] && v[k+1] == v[k+2] && v[k+2] != v[k+3]
+                s2 += any(p.(1:5))
+            end
+        end
+    end
+    (s1, s2)
+end
+
+data = parse.(Int, split(readline("input4.txt"), "-"))
+
+@show countpasswords(data[1]:data[2])
